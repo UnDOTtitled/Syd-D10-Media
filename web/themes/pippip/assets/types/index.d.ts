8a5e4698e5fe3ba7cefbbb0450c8af35d3a2d7eb
Angelou <angelou.jeffers@un.titled.co.uk>
2024-03-18 15:48:52 +0000

    [theme-updates] - declare YT namespacing for typescript

--- web/themes/pippip/assets/types/index.d.ts
+++ web/themes/pippip/assets/types/index.d.ts
@@ -2,3 +2,10 @@ declare module 'baguettebox.js'
 declare module 'van11y-accessible-accordion-aria'
 declare module 'vanilla-lazyload'
 declare module 'focus-within'
+
+// yt.d.ts
+declare namespace YT {
+    class Player {
+      constructor(id: string, options: any);
+    }
+  }
